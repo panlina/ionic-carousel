3733634ff4d2ca59c6cdf6f104ec89d2d5773300
Jack Lu <jacklu@jacklu.me>
2017-03-02 12:27:14 +0800

    +ng-model

--- carousel.js
+++ carousel.js
@@ -1,37 +1,43 @@
 ﻿angular.module('carousel', ['ionic'])
 	.directive('carousel', function ($ionicGesture) {
 		return {
-			compile: function (element) {
+			scope: {
+				ngModel: '='
+			},
+			compile: function (element, attr) {
 				var width = element.prop('offsetWidth');
-				var n = element.children().length;
-				var children = element.children()
-					.wrap('<div>')
+				var strip = element.children()
+					.wrap('<div ng-repeat="$item in ' + attr.ngModel + '" style=width:' + width + 'px;>')
 					.parent()
-					.css('width', width + 'px');
-				var strip = angular.element("<div>")
-					.append(children)
-					.css('width', width * n + 'px')
+					.wrap("<div>")
+					.parent()
 					.css({ 'transition-property': 'transform' });
-				element.append(strip);
-				var x = 0;
-				$ionicGesture.on('drag', function ($event) {
-					translate(x + $event.gesture.deltaX);
-				}, strip);
-				$ionicGesture.on('dragend', function ($event) {
-					x = width * nearest(x + $event.gesture.deltaX);
-					translate(x, .4);
-				}, strip);
-				function translate(x, duration) {
-					strip.css({ 'transition-duration': (duration || 0) + 's' });
-					strip.css({ transform: "translateX(" + x + "px)" });
-				}
-				function nearest(x) {
-					x /= width;
-					if (x > 0) return 0;
-					if (x < -(n - 1)) return -(n - 1);
-					var f = Math.floor(x);
-					return x < f + .5 ? f : (f + 1);
-				}
+				return function (scope, element) {
+					var x = 0;
+					$ionicGesture.on('drag', function ($event) {
+						translate(x + $event.gesture.deltaX);
+					}, strip);
+					$ionicGesture.on('dragend', function ($event) {
+						x = width * nearest(x + $event.gesture.deltaX);
+						translate(x, .4);
+					}, strip);
+					scope.$watch('ngModel', function (value) {
+						if (!value) return;
+						element.children().css('width', width * value.length + 'px');
+					});
+					function translate(x, duration) {
+						strip.css({ 'transition-duration': (duration || 0) + 's' });
+						strip.css({ transform: "translateX(" + x + "px)" });
+					}
+					function nearest(x) {
+						var n = scope.ngModel.length;
+						x /= width;
+						if (x > 0) return 0;
+						if (x < -(n - 1)) return -(n - 1);
+						var f = Math.floor(x);
+						return x < f + .5 ? f : (f + 1);
+					}
+				};
 			}
 		};
 	})
